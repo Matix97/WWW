370333ba0b04ce5d15d7e4b58735c2cc42927773
Matix97 <mateusz_baranski@outlook.com>
2019-05-11 17:31:10 +0200

    adding new users

--- js/app.js
+++ js/app.js
@@ -14,8 +14,6 @@ myApp.run(function(editableOptions) {
 // Tworzymy kotroler UserCtrl
 myApp.controller('UserCtrl', ['$scope', '$http', function ($scope, $http) {
 
-
-
    // tworzymy model
    $scope.users = [];
    $scope.newUser = {};
@@ -29,44 +27,19 @@ myApp.controller('UserCtrl', ['$scope', '$http', function ($scope, $http) {
       var newUser = $scope.newUser;
       newUser.state = "normal";
       newUser.index = $scope.users.length;
+      newUser.javaScript=0;
+      newUser.jQuery=0;
+      newUser.PHP=0;
       $scope.users.push(newUser);
-      $scope.newUser = {};
+      $scope.newUser = {};      
    };
 
    $scope.deleteUser = function (user) {
-      if (user.state == "deleted") {
          $scope.users.splice(user.index, 1);
          _setIndexes();
-      } else {
-         user.state = "deleted";
-      }
    };
 
-   // $scope.undoDelete = function (user) {
-   //    user.state = "normal";
-   // };
-   $scope.editUser = function (user) {
-      user.oldNazwiskoIImie = user.nazwiskoIImie;
-      user.oldNr_indeksu = user.nr_indeksu;
-      user.oldJavaScript = user.javaScript;
-      user.oldJQuery = user.jQuery;
-      user.oldPHP = user.PHP;
-      user.state = "edit";
-   };
-
-   $scope.saveUser = function (user) {
-      // tu możemy użyć Ajaxowego  POST do zapisu danych 
-      user.state = "normal";
-   };
-
-   $scope.cancelEdit = function (user) {
-      user.nazwiskoIImie = user.oldNazwiskoIImie;
-      user.nr_indeksu = user.oldNr_indeksu;
-      user.javaScript = user.oldJavaScript;
-      user.jQuery = user.oldJQuery;
-      user.PHP = user.oldPHP;
-      user.state = "normal";
-   };
+   
 
    // metody prywatne
    function _setIndexes() {
